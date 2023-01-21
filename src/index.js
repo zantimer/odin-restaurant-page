679ac25532e994ab16f5ad2116a4155147ce1994
zantimer <karol.nikiel1@gmail.com>
2023-01-21 10:04:37 +0000

    basic setup complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('i work');
+import './style.css';
+
+const mainDiv = document.querySelector('.content');
+
+function divMaker(name){
+    const div = document.createElement('div');
+    div.classList.add(name);
+    mainDiv.appendChild(div);
+}
+
+divMaker('header');
+divMaker('logo');
+divMaker('writeup');
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,23 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devtool: "inline-source-map",
+    devServer: {
+        static: 'dist',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    // optimization: {
+    //     runtimeChunk: 'single',
+    // },
 }
